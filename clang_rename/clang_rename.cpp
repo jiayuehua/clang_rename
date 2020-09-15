210ed400e808e1e19d4d42acc50e34acc9d7ddfd
rob <jiayuehua@jd.com>
2020-09-16 07:38:50 +0800

    vcpkg binary cache

---
+++ clang_rename/clang_rename.cpp
@@ -0,0 +1,19 @@
+// clang_rename.cpp : Defines the entry point for the application.
+//
+
+#include "clang_rename.h"
+
+using namespace std;
+void hello()
+{
+  int boy = 1;
+  int b = boy + 5;
+  int c = boy + 5;
+  cout << boy << c << endl;
+}
+
+int main()
+{
+  cout << "Hello CMake." << endl;
+  return 0;
+}
